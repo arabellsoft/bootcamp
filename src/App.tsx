20711e50910c37053d830ad6f796d33dd52ffa42
Peter Akarakiri <p.akarakiri@gmail.com>
2023-04-11 09:58:35 -0700

    added an example of flex container with a column

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,15 @@ function App() {
       <p className="title">
         Scores Preview 
       </p>
+
+      <div className="flexContainer">
+        <div className="row">
+          <div className="flexItem">Friday, Mar 31</div>
+          <div className="flexItem">Logo</div>
+          <div className="flexItem">Flames</div>
+          <div className="flexItem">32-26-15</div>
+        </div>
+      </div>
     </div>
   );
 }
